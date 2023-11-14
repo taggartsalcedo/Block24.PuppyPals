0234e320717a104a959dbdbef16cd023e5fc22d1
Taggart <t.salcedo@hotmail.com>
2023-11-14 04:06:32 -0500

    third updat

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import { puppyList } from './data.js'
 import { useState } from 'react' 
 import './App.css' 
 
-const App = () => {
+function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  
-  console.log(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+
+function handleClick() {
+  //some logic here
+}
 
   return (
-    <div>
-      {
-        puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
-        })
-      }
+    <div className="App">
+      {puppies.map((puppy) => {
+          return (
+             <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+              </p>
+          );
+      })}
     </div>
-  )
+  );
 }
 
 export default App

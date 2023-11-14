b986a75abee7fcd5ae21b79042fef9486657ed95
Taggart <t.salcedo@hotmail.com>
2023-11-14 03:36:25 -0500

    second updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react' 
+import './App.css' 
 
-function App() {
+const App = () => {
+  const [puppies, setPuppies] = useState(puppyList)
+  
+  console.log(puppyList);
 
   return (
     <div>
+      {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+      }
     </div>
   )
 }
